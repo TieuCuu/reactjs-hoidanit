479e7fc0fef3609b72def84134f25cc273943ba7
PhatLy <ldphat99@gmail.com>
2022-12-16 15:42:32 +0700

    class component & function component

--- src/App.js
+++ src/App.js
@@ -3,31 +3,43 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponents.js';
+import React from 'react';
 
+//cách viết class component
+class App extends React.Component {
+	render() {
+		return (
 
-const App = () => {
-	const count = useSelector(state => state.counter.count);
-	const dispatch = useDispatch();
+			<div>
+				hello world
+				<MyComponent></MyComponent>
+			</div>
 
-	return (
-		// <div className="App">
-		// 	<header className="App-header">
-		// 		<img src={logo} className="App-logo" alt="logo" />
-		// 		<p>
-		// 			hello world
-		// 		</p>
-		// 		<div>Count = {count}</div>
-		// 		<button onClick={() => dispatch(increaseCounter())}>Increase</button>
-		// 		<button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-		// 	</header>
-		// </div>
-
-		<div>
-			hello world
-			<MyComponent></MyComponent>
-		</div>
-
-	);
+		)
+	}
 }
+
+//cách viết function component
+// const App = () => {
+// 	const count = useSelector(state => state.counter.count);
+// 	const dispatch = useDispatch();
+
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<img src={logo} className="App-logo" alt="logo" />
+// 				<p>
+// 					hello world
+// 				</p>
+// 				<div>Count = {count}</div>
+// 				<button onClick={() => dispatch(increaseCounter())}>Increase</button>
+// 				<button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+// 			</header>
+// 		</div>
+
+
+
+// 	);
+// }
 
 export default App;
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -5,10 +5,21 @@ import React from "react";
 
 class MyComponent extends React.Component {
 
+    //state là 1 biến object, dữ liệu state thay đổi thì trên trình duyệt thay đổi theo
+    state = {
+        name: 'Phat',
+        address: 'Ha Noi',
+        age: 18
+
+    }
+
     //JSX
     render() {
         return (
-            <div>my first component</div>
+            <div>
+
+                My name is {this.state.name} and I'm from {this.state.address}
+            </div>
         )
     }
 }
