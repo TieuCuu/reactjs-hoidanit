21de8af9dee9afa8b96b87472771ba15d2742f3d
PhatLy <ldphat99@gmail.com>
2022-12-22 10:45:03 +0700

    convert class to function component with hooks

--- src/components/AddUserInfor.js
+++ src/components/AddUserInfor.js
@@ -1,53 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 
-class AddUserInfor extends React.Component {
+// class AddUserInfor extends React.Component {
 
-    //state là 1 biến object, dữ liệu state thay đổi thì trên trình duyệt thay đổi theo
-    state = {
-        name: '',
-        address: 'Ha Noi',
-        age: ''
+//     //state là 1 biến object, dữ liệu state thay đổi thì trên trình duyệt thay đổi theo
+//     state = {
+//         name: '',
+//         address: 'Ha Noi',
+//         age: ''
 
+//     }
+
+//     handleOnChangeInput = (event) => {
+//         this.setState({
+//             name: event.target.value
+//         })
+//     }
+
+//     handleOnChangeAge = (event) => {
+//         this.setState({
+//             age: event.target.value
+//         })
+//     }
+
+//     handleOnSubmit = (event) => {
+//         event.preventDefault();
+//         this.props.handleAddNewUser({
+//             id: (Math.floor(Math.random() * 100) + 1) + '-random',
+//             name: this.state.name,
+//             age: this.state.age
+//         });
+//         //Thực thi props là 1 function thì phải có ()
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 My name is {this.state.name} and I'm {this.state.age}
+//                 <form action="" onSubmit={(event) => this.handleOnSubmit(event)}>
+//                     <label htmlFor="">Your name:</label>
+//                     <input type="text" value={this.state.name} onChange={(event) => this.handleOnChangeInput(event)} />
+
+//                     <label htmlFor="">Your age:</label>
+//                     <input type="text" value={this.state.age} onChange={(event) => this.handleOnChangeAge(event)} />
+
+//                     <button>Submit</button>
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
+
+const AddUserInfor = (props) => {
+
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('Ha Noi');
+    const [age, setAge] = useState('');
+
+
+    const handleOnChangeInput = (event) => {
+        setName(event.target.value);
     }
 
-    handleOnChangeInput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
+    const handleOnChangeAge = (event) => {
+        setAge(event.target.value);
     }
 
-    handleOnChangeAge = (event) => {
-        this.setState({
-            age: event.target.value
-        })
-    }
-
-    handleOnSubmit = (event) => {
+    const handleOnSubmit = (event) => {
         event.preventDefault();
-        this.props.handleAddNewUser({
+        props.handleAddNewUser({
             id: (Math.floor(Math.random() * 100) + 1) + '-random',
-            name: this.state.name,
-            age: this.state.age
+            name: name,
+            age: age
         });
-        //Thực thi props là 1 function thì phải có ()
+
+
     }
 
-    render() {
-        return (
-            <div>
-                My name is {this.state.name} and I'm {this.state.age}
-                <form action="" onSubmit={(event) => this.handleOnSubmit(event)}>
-                    <label htmlFor="">Your name:</label>
-                    <input type="text" value={this.state.name} onChange={(event) => this.handleOnChangeInput(event)} />
+    return (
+        <div>
+            My name is {name} and I'm {age}
+            <form action="" onSubmit={(event) => handleOnSubmit(event)}>
+                <label htmlFor="">Your name:</label>
+                <input type="text" value={name} onChange={(event) => handleOnChangeInput(event)} />
 
-                    <label htmlFor="">Your age:</label>
-                    <input type="text" value={this.state.age} onChange={(event) => this.handleOnChangeAge(event)} />
+                <label htmlFor="">Your age:</label>
+                <input type="text" value={age} onChange={(event) => handleOnChangeAge(event)} />
 
-                    <button>Submit</button>
-                </form>
-            </div>
-        )
-    }
+                <button>Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default AddUserInfor;
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -3,53 +3,89 @@
 //DRY: don't repeat yourself: nghĩa là những việc lặp lại thì chắc chắn có cách tối ưu
 
 
-import React from "react";
+import React, { useState } from "react";
 import DisplayInfor from "./DisplayInfor";
 import AddUserInfor from "./AddUserInfor";
 
-class MyComponent extends React.Component {
+// class MyComponent extends React.Component {
 
 
-    state = {
-        listUsers: [
+//     state = {
+//         listUsers: [
+//             { id: 1, name: "Xiao Jiu", age: '18' },
+//             { id: 2, name: "Tiểu Cửu", age: '20' },
+//             { id: 3, name: "Bảo Nghi", age: '24' }
+//         ]
+//     }
+
+//     handleAddNewUser = (userObj) => {
+//         this.setState({
+//             listUsers: [userObj, ...this.state.listUsers]
+//         })
+//     }
+
+//     handleDeleteUser = (userId) => {
+//         let listUsersClone = [...this.state.listUsers];
+//         listUsersClone = listUsersClone.filter(item => item.id !== userId);
+//         this.setState({
+//             listUsers: listUsersClone
+//         })
+//     }
+
+//     //JSX
+//     render() {
+//         const myInfor = ['ab', 'c', 'd'];
+//         return (
+//             <>
+//                 <div className="a">
+//                     <AddUserInfor handleAddNewUser={this.handleAddNewUser} />
+//                     {/* handleAddNewUser k có dấu () nghĩa là tham chiếu đến function, có () là thực thi function */}
+//                     {/* Truyền tham chiếu cho props là function thì k cần () */}
+//                     <br /> <br />
+
+//                     <DisplayInfor listUsers={this.state.listUsers} handleDeleteUser={this.handleDeleteUser} />
+//                 </div>
+
+//                 <div className="b"></div>
+//             </>
+//         )
+//     }
+// }
+
+const MyComponent = (props) => {
+
+    const [listUsers, setListUsers] = useState(
+        [
             { id: 1, name: "Xiao Jiu", age: '18' },
             { id: 2, name: "Tiểu Cửu", age: '20' },
             { id: 3, name: "Bảo Nghi", age: '24' }
         ]
+    );
+
+    const handleAddNewUser = (userObj) => {
+        setListUsers([userObj, ...listUsers]);
     }
 
-    handleAddNewUser = (userObj) => {
-        this.setState({
-            listUsers: [userObj, ...this.state.listUsers]
-        })
-    }
-
-    handleDeleteUser = (userId) => {
-        let listUsersClone = [...this.state.listUsers];
+    const handleDeleteUser = (userId) => {
+        let listUsersClone = listUsers;
         listUsersClone = listUsersClone.filter(item => item.id !== userId);
-        this.setState({
-            listUsers: listUsersClone
-        })
+        setListUsers(listUsersClone);
     }
 
-    //JSX
-    render() {
-        const myInfor = ['ab', 'c', 'd'];
-        return (
-            <>
-                <div className="a">
-                    <AddUserInfor handleAddNewUser={this.handleAddNewUser} />
-                    {/* handleAddNewUser k có dấu () nghĩa là tham chiếu đến function, có () là thực thi function */}
-                    {/* Truyền tham chiếu cho props là function thì k cần () */}
-                    <br /> <br />
 
-                    <DisplayInfor listUsers={this.state.listUsers} handleDeleteUser={this.handleDeleteUser} />
-                </div>
+    return (
+        <>
+            <div className="a">
+                <AddUserInfor handleAddNewUser={handleAddNewUser} />
+                <br /> <br />
 
-                <div className="b"></div>
-            </>
-        )
-    }
+                <DisplayInfor listUsers={listUsers} handleDeleteUser={handleDeleteUser} />
+            </div>
+
+            <div className="b"></div>
+        </>
+    )
 }
+
 
 export default MyComponent
